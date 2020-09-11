05ef7315b20236d7964bce60430060d4cbbc4df9
Nestor <ncervan87@gmail.com>
2020-09-11 09:03:35 -0700

    add db schema

---
+++ data/migrations/20200911084052_projects.js
@@ -0,0 +1,29 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('project', tbl => {
+        tbl.increments()
+        tbl.string('name').notNull()
+        tbl.string('description')
+        tbl.boolean('complete').defaultTo(0)
+    }).createTable('resource', tbl => {
+        tbl.increments()
+        tbl.string('name').notNull()
+        tbl.string('description')
+    }).createTable('task', tbl => {
+        tbl.increments()
+        tbl.string('description').notNull()
+        tbl.string('notes')
+        tbl.boolean('complete').defaultTo(0)
+    }).createTable('project_resource', tbl => {
+        tbl.integer('project_id').references('id').inTable('project').notNull().onDelete('CASCADE').onUpdate('CASCADE')
+        tbl.integer('resource_id').references('id').inTable('resource').notNull().onDelete('CASCADE').onUpdate('CASCADE')
+        tbl.primary(['project_id', 'resource_id'])
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('project_resource')
+        .dropTableIfExists('task')
+        .dropTableIfExists('resource')
+        .dropTableIfExists('project')
+};
--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './projects.db3'
+      filename: './data/projects.db3'
     },
     migrations: {
       directory: './data/migrations'
