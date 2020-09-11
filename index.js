29d271b539b3bde1a1f3ad551fcccb208daf0efc
Nestor <ncervan87@gmail.com>
2020-09-11 08:36:21 -0700

    add express router

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require('./api/server')
 
 const port = process.env.PORT || 8000
 
+const projectRouter = require('./projects/projects-router')
+
+app.use('/api/projects', projectRouter)
+
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`)
